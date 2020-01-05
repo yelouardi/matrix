18693bde261b92c96a980e041cd9177bc2305e03
Yassine EL OUARDI <yelouardi@sqli.com>
2020-01-06 00:13:49 +0100

    [DATA] Change To Mysqli with Master and salave

--- src/test/java/com/humanup/matrix/MatrixApplicationTests.java
+++ src/test/java/com/humanup/matrix/MatrixApplicationTests.java
@@ -1,18 +1,14 @@
 package com.humanup.matrix;
 
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class MatrixApplicationTests {
 
 @Test
- @DatabaseSetup("person.xml")
  public void testFind() throws Exception {
 
  }
