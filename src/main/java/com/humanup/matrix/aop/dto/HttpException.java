cb775eb5109d03e13cd5eb405fd81ae5da8bda01
Yassine EL OUARDI <yelouardi@sqli.com>
2020-05-06 13:58:47 +0200

    Add AOP for Exception and HTTP

---
+++ src/main/java/com/humanup/matrix/aop/dto/HttpException.java
@@ -0,0 +1,12 @@
+package com.humanup.matrix.aop.dto;
+
+public class HttpException extends Exception {
+
+    public HttpException(String message) {
+        super(message);
+    }
+
+    public HttpException() {
+        super();
+    }
+}
---
+++ src/main/java/com/humanup/matrix/aop/dto/TypeSkillException.java
@@ -0,0 +1,16 @@
+package com.humanup.matrix.aop.dto;
+
+public class TypeSkillException extends HttpException {
+    public TypeSkillException(String message) {
+        super(message);
+    }
+
+    public TypeSkillException() {
+        super();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Cannot Create TypeSkill";
+    }
+}
